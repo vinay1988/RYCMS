37f3b5092380b85a291e93be5964a7a69e727334
vinay kumar mishra <vinay.mishra@live.com>
2018-05-25 10:59:51 +0530

    aa

---
+++ RYWebsite/Controllers/Executive/ExecutiveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RYWebsite.Controllers.Executive
+{
+    public class ExecutiveController : Controller
+    {
+        // GET: Executive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RYWebsite/Models/Executive/ExecutiveModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using System.Web.Script.Serialization;
+
+namespace RYWebsite.Models
+{
+    [SitecoreType(AutoMap = true)]
+    public class ExecutiveModel
+    {
+
+        [SitecoreChildren(InferType = true)]
+        //public  List<FooterItems> Children { get; set; }
+        public IEnumerable<MediaCardItem> Children { get; set; }
+
+    }
+}
